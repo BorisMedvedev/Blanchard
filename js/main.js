8030478cdd8816f2c3cba06dc4f8995c5e232471
BorisMedvedev <70233602+BorisMedvedev@users.noreply.github.com>
2020-12-13 21:05:28 +0300

    swiper

--- js/main.js
+++ js/main.js
@@ -1,43 +1,24 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 300,
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-
+var mySwiper = new Swiper('.swiper-1', {
+	loop: true,
+	speed: 300,
+	watchSlidesProgress: true,
+	slidesPerView: 3,
+	slidesPerColumn: 2,
+	slidesPerColumnFill: "row",
+	slidesPerGroup: 3,
+	spaceBetween: 50,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	}
 });
-
 
 
 /*select========================*/
 
 const element = document.querySelector('#selectCostom');
 const choices = new Choices(element, {
-    searchEnabled: false
+	searchEnabled: false
 });
 
-/*==================swiper-container2========================*/
-new Swiper('.galleri-swiper__swiper', {
-    direction: 'horizontal',
-    loop: true,
-    speed: 300,
-    slidesPerView: 1,
-    slidesPerColumn: 3,
-    spaceBetween: 0,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-});
+
