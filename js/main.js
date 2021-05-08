c69bcc9c297aed42346c5025d2ef52ed93384972
BorisMedvedev <70233602+BorisMedvedev@users.noreply.github.com>
2021-05-08 21:39:07 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+const button = document.querySelectorAll('button');
+const drop = document.querySelectorAll('.dropdown')
+
+button.forEach(el => {
+  el.addEventListener('click', (e) => {
+    drop.forEach(el => {el.classList.remove(('dropdown--active'))})
+    e.currentTarget.closest('li').querySelector('.dropdown').classList.toggle('dropdown--active');
+  });
+});
+
+document.addEventListener('click', (e) => {
+  console.log(e.target)
+  if (!e.target.classList.contains('dropdown') && !e.target.classList.contains('drop-btn')) {
+    drop.forEach(el => {el.classList.remove(('dropdown--active'))})
+  }
+});
+
+
+/* swiper=================================================*/
+
 var mySwiper = new Swiper('.swiper-1', {
 	loop: true,
 	speed: 300,
