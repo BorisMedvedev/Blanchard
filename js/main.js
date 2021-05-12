a6e02c4a40947808ff084da404f51400dcdf65cb
BorisMedvedev <70233602+BorisMedvedev@users.noreply.github.com>
2021-05-12 21:55:02 +0300

    2154

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const drop = document.querySelectorAll('.dropdown')
 
 button.forEach(el => {
   el.addEventListener('click', (e) => {
-    drop.forEach(el => {el.classList.remove(('dropdown--active'))})
+    drop.forEach(el => { el.classList.remove(('dropdown--active')) })
     e.currentTarget.closest('li').querySelector('.dropdown').classList.toggle('dropdown--active');
   });
 });
@@ -11,7 +11,7 @@ button.forEach(el => {
 document.addEventListener('click', (e) => {
   console.log(e.target)
   if (!e.target.classList.contains('dropdown') && !e.target.classList.contains('drop-btn')) {
-    drop.forEach(el => {el.classList.remove(('dropdown--active'))})
+    drop.forEach(el => { el.classList.remove(('dropdown--active')) })
   }
 });
 
@@ -19,22 +19,22 @@ document.addEventListener('click', (e) => {
 /* swiper=================================================*/
 
 var mySwiper = new Swiper('.swiper-1', {
-	loop: true,
-	speed: 300,
-	watchSlidesProgress: true,
-	slidesPerView: 3,
-	slidesPerColumn: 2,
-	slidesPerColumnFill: "row",
-	slidesPerGroup: 3,
-	spaceBetween: 50,
-	pagination: {
-		el: ".swiper-pagination",
-		type: "fraction",
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	}
+  speed: 300,
+  watchSlidesProgress: true,
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  slidesPerColumnFill: "row",
+  slidesPerGroup: 6,
+  spaceBetween: 50,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: '.swiper-button-prev',
+    prevEl: '.swiper-button-next',
+  }
 });
 
 
@@ -42,7 +42,7 @@ var mySwiper = new Swiper('.swiper-1', {
 
 const element = document.querySelector('#selectCostom');
 const choices = new Choices(element, {
-	searchEnabled: false
+  searchEnabled: false
 });
 
 
