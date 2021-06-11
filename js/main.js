1af3899a83b57ff1f8374c6ba18435d53fc7576b
BorisMedvedev <70233602+BorisMedvedev@users.noreply.github.com>
2021-06-11 17:22:56 +0300

    999

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-const button = document.querySelectorAll('button');
-const drop = document.querySelectorAll('.nav-top')
+const button = document.querySelectorAll("button");
+const drop = document.querySelectorAll(".nav-top");
 
-button.forEach(el => {
-  el.addEventListener('click', (e) => {
-    drop.forEach(el => { el.classList.remove(('nav-top--active')) })
-    e.currentTarget.closest('li').querySelector('.nav-top').classList.toggle('nav-top--active');
+button.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    drop.forEach((el) => {
+      el.classList.remove("nav-top--active");
+    });
+    e.currentTarget
+      .closest("li")
+      .querySelector(".nav-top")
+      .classList.toggle("nav-top--active");
   });
 });
 
-document.addEventListener('click', (e) => {
-  console.log(e.target)
-  if (!e.target.classList.contains('nav-top') && !e.target.classList.contains('drop-btn')) {
-    drop.forEach(el => { el.classList.remove(('nav-top--active')) })
+document.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (
+    !e.target.classList.contains("nav-top") &&
+    !e.target.classList.contains("drop-btn")
+  ) {
+    drop.forEach((el) => {
+      el.classList.remove("nav-top--active");
+    });
   }
 });
 /* swiper=================================================*/
@@ -31,22 +41,19 @@ var swiper = new Swiper(".myswiper", {
     type: "fraction",
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-
 /*select========================*/
 
-const element = document.querySelector('#selectCostom');
+const element = document.querySelector("#selectCostom");
 const choices = new Choices(element, {
-  searchEnabled: false
+  searchEnabled: false,
 });
 
-
 /*Accordeon========================*/
-
 
 $(function () {
   $(".accordion1").accordion();
@@ -61,29 +68,32 @@ $(function () {
 });
 /*tabs========================*/
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) {
-
-    tabsBtn.addEventListener('click', function (event) {
-      const path = event.currentTarget.dataset.path
-      console.log(path)
-      document.querySelectorAll('.tab-content').forEach(function (tabContent) {
-        tabContent.classList.remove('tab-content-active')
-      })
-      document.querySelectorAll('.catalog-block__disabled').forEach(function (tabContent) {
-        tabContent.classList.remove('catalog-block__active')
-      })
-      document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active')
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".tabs__btn").forEach(function (tabsBtn) {
+    tabsBtn.addEventListener("click", function (event) {
+      const path = event.currentTarget.dataset.path;
+      console.log(path);
+      document.querySelectorAll(".tab-content").forEach(function (tabContent) {
+        tabContent.classList.remove("tab-content-active");
+      });
+      document
+        .querySelectorAll(".catalog-block__disabled")
+        .forEach(function (tabContent) {
+          tabContent.classList.remove("catalog-block__active");
+        });
+      document
+        .querySelector(`[data-target="${path}"]`)
+        .classList.add("tab-content-active");
       // console.log(tab - content - acive)
       // e.currentTarget.classList.add('tab-content-active')
-    })
-
-  })
-
-})
-document.querySelectorAll('.tabs__btn').forEach(el => {
-  el.addEventListener('click', (e) => {
-    document.querySelectorAll('.tabs__btn').forEach(el => { el.classList.remove('catalog-block__active') });
-    e.currentTarget.classList.add('catalog-block__active');
+    });
+  });
+});
+document.querySelectorAll(".tabs__btn").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    document.querySelectorAll(".tabs__btn").forEach((el) => {
+      el.classList.remove("catalog-block__active");
+    });
+    e.currentTarget.classList.add("catalog-block__active");
   });
 });
